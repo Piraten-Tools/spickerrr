6acacc8934da2766cd06533a46cfce1673357266
LostInCoding <f.wosti@gmail.com>
2016-05-26 13:53:13 +0200

    Added Antrag

--- app/src/main/java/de/lostincoding/spickerrr2/model/Antrag.java
+++ app/src/main/java/de/lostincoding/spickerrr2/model/Antrag.java
@@ -4,4 +4,14 @@ package de.lostincoding.spickerrr2.model;
  * Created by lostincoding on 26.05.16.
  */
 public class Antrag {
+    private String id;
+    private String title;
+    private String topic;
+    private String kind;
+    private String owner;
+    private String infourl;
+    private String abstract_short; //abstract is forbidden
+    private String description;
+    private String motivation;
+    
 }
