c42fca43e4cdb1868db2876a82b16cebeb5d1d08
LostInCoding <f.wosti@gmail.com>
2016-06-14 16:32:36 +0200

    Added parse json

--- app/src/main/java/de/lostincoding/spickerrr2/api/AntragsAPI.java
+++ app/src/main/java/de/lostincoding/spickerrr2/api/AntragsAPI.java
@@ -3,6 +3,8 @@ package de.lostincoding.spickerrr2.api;
 import android.content.Context;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -29,8 +31,30 @@ public class AntragsAPI {
         return response.body().string();
     }
 
-    private ArrayList<Antrag> parseJSON(String data) {
-        JSONObject reader
+    private ArrayList<Antrag> parseJSON(String data, Package insertpackage) {
+        ArrayList<Antrag> antragsliste = new ArrayList<>();
+        JSONArray antragsArray = null;
+        try {
+            antragsArray = new JSONArray(data);
+            for (int i = 0; i < 10; i++) {
+                JSONObject aktobject = antragsArray.getJSONObject(i);
+                String id = (String) aktobject.get(insertpackage.getColId());
+                String title = (String) aktobject.get(insertpackage.getColTitle());
+                String topic = (String) aktobject.get(insertpackage.getColTopic());
+                String kind = (String) aktobject.get(insertpackage.getColKind());
+                String owner = (String) aktobject.get(insertpackage.getColOwner());
+                String infourl = (String) aktobject.get(insertpackage.getColInfoUrl());
+                String abstract_short = (String) aktobject.get(insertpackage.getColAbstract());
+                String description = (String) aktobject.get(insertpackage.getColDescription());
+                String motivation = (String) aktobject.get(insertpackage.getColMotivation());
+                antragsliste.add(new Antrag(id, title, topic, kind, owner, infourl, abstract_short, description, motivation));
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return antragsliste;
+
     }
 
     private ArrayList<Antrag> parseCSV(String data) {
@@ -49,7 +73,7 @@ public class AntragsAPI {
         }
         switch (insertpackage.getSourceType()) {
             case "JSON":
-                antragslist = parseJSON(data);
+                antragslist = parseJSON(data, insertpackage);
                 break;
             case "CSV":
                 antragslist = parseCSV(data);
--- app/src/main/java/de/lostincoding/spickerrr2/model/Antrag.java
+++ app/src/main/java/de/lostincoding/spickerrr2/model/Antrag.java
@@ -14,4 +14,51 @@ public class Antrag {
     private String description;
     private String motivation;
 
+    public Antrag(String id, String title, String topic, String kind, String owner, String infourl, String abstract_short, String description, String motivation) {
+        this.id = id;
+        this.title = title;
+        this.topic = topic;
+        this.kind = kind;
+        this.owner = owner;
+        this.infourl = infourl;
+        this.abstract_short = abstract_short;
+        this.description = description;
+        this.motivation = motivation;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public String getInfourl() {
+        return infourl;
+    }
+
+    public String getAbstract_short() {
+        return abstract_short;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getMotivation() {
+        return motivation;
+    }
 }
