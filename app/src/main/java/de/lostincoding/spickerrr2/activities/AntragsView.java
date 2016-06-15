eabada30605817bae8e5b0720a0beb63d3a1c96c
LostInCoding <f.wosti@gmail.com>
2016-06-15 13:29:20 +0200

    Deleted duplicated file

--- app/src/main/java/de/lostincoding/spickerrr2/activities/AntragsView.java
+++ app/src/main/java/de/lostincoding/spickerrr2/activities/AntragsView.java
@@ -10,6 +10,6 @@ public class AntragsView extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_antrags_view2);
+        setContentView(R.layout.activity_antrags_view);
     }
 }
