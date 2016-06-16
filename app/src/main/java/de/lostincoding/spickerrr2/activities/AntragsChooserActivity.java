0e82d4f6a172fe13e12ab12585bd58ac21fded83
LostInCoding <f.wosti@gmail.com>
2016-06-16 16:39:59 +0200

    Created own file for viewpageradapter

--- app/src/main/java/de/lostincoding/spickerrr2/activities/AntragsChooserActivity.java
+++ app/src/main/java/de/lostincoding/spickerrr2/activities/AntragsChooserActivity.java
@@ -133,35 +133,6 @@ public class AntragsChooserActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-
-    class ViewPagerAdapter extends FragmentPagerAdapter {
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        private final List<String> mFragmentTitleList = new ArrayList<>();
-
-        public ViewPagerAdapter(FragmentManager manager) {
-            super(manager);
-        }
-
-        @Override
-        public android.support.v4.app.Fragment getItem(int position) {
-            return mFragmentList.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return mFragmentList.size();
-        }
-
-        public void addFragment(Fragment fragment, String title) {
-            mFragmentList.add(fragment);
-            mFragmentTitleList.add(title);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitleList.get(position);
-        }
-    }
-
+    
 }
 
