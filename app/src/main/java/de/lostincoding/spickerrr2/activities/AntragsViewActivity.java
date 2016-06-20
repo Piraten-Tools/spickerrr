69b1b16237751758893789b11b907a465d368742
lostincoding <f.wosti@gmail.com>
2016-06-20 17:23:41 +0200

    added author and  topic to antrags view

--- app/src/main/java/de/lostincoding/spickerrr2/activities/AntragsViewActivity.java
+++ app/src/main/java/de/lostincoding/spickerrr2/activities/AntragsViewActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 import android.webkit.WebView;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import de.lostincoding.spickerrr2.R;
 import de.lostincoding.spickerrr2.model.Antrag;
 
@@ -15,6 +17,8 @@ public class AntragsViewActivity extends AppCompatActivity {
     private Antrag antrag;
     private WebView description;
     private WebView motivation;
+    private TextView topic;
+    private TextView author;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +37,12 @@ public class AntragsViewActivity extends AppCompatActivity {
 
     private void initalizeUI() {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
+        setTitle(antrag.getId() + " " + antrag.getTitle());
 
         description = (WebView) findViewById(R.id.description);
         motivation = (WebView) findViewById(R.id.motivation);
-        setTitle(antrag.getId() + " " + antrag.getTitle());
+        author = (TextView) findViewById(R.id.author);
+        topic = (TextView) findViewById(R.id.topic);
 
         description.getSettings();
         description.setBackgroundColor(Color.TRANSPARENT);
@@ -48,6 +53,8 @@ public class AntragsViewActivity extends AppCompatActivity {
 
         description.loadData(antrag.getDescription(), "text/html; charset=UTF-8", null);
         motivation.loadData(antrag.getMotivation(), "text/html; charset=UTF-8", null);
+        author.setText(antrag.getOwner());
+        topic.setText(antrag.getTopic());
 
 
     }
