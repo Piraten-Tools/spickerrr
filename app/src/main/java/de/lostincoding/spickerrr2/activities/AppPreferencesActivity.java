07fd45bf8ebf6bfd03686ec00df24b12a29c2cd5
lostincoding <f.wosti@gmail.com>
2016-06-29 22:11:57 +0200

    added title and back button for preferences

--- app/src/main/java/de/lostincoding/spickerrr2/activities/AppPreferencesActivity.java
+++ app/src/main/java/de/lostincoding/spickerrr2/activities/AppPreferencesActivity.java
@@ -3,6 +3,7 @@ package de.lostincoding.spickerrr2.activities;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import de.lostincoding.spickerrr2.fragments.AppPreferenceFragment;
 
@@ -14,6 +15,13 @@ public class AppPreferencesActivity extends AppCompatActivity implements Prefere
         // Display the fragment as the main content.
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new AppPreferenceFragment()).commit();
+        setTitle("Einstellungen");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        finish();
+        return true;
     }
 
     @Override
